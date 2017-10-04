f1ee6014de1dac4d8310f4a193a6319207faa2e2
janzheng <janeazy@gmail.com>
2017-10-04 08:26:10 -0400

    updates to layout

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -135,9 +135,9 @@ function onResize() {
   }
 
   // for drawing grids for _grid-markers
-  var mainWidth = $('._grid-markers-basis').css( "width" );
+  var mainWidth = parseInt($('._grid-markers-basis').css( "width" )) - parseInt($('._grid-markers-basis').css( "padding-right" )) - parseInt($('._grid-markers-basis').css( "padding-left" ));
   var cols = $('._grid-markers').data('cols');
-  // console.log('grid marker width: ', mainWidth, ' cols', cols, $('._grid-markers').innerWidth());
+  console.log('grid marker width: ', mainWidth, ' cols', cols, $('._grid-markers').innerWidth());
 
   var emptyDivs = '';
   for(i=0; i<cols; i++) {
@@ -147,7 +147,11 @@ function onResize() {
     .html(emptyDivs)
     .width(mainWidth)
     .addClass('_span-col-'+cols)
-    .css({'grid-template-columns': 'repeat('+cols+', 1fr)'});
+    .css({
+      'grid-template-columns': 'repeat('+cols+', 1fr)',
+      'margin-left': $('._grid-markers-basis').css( "padding-left" ),
+      'margin-right': $('._grid-markers-basis').css( "padding-right" ),
+    });
 
 }
 
