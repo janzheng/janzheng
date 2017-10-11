0b65cec03abcd408626ff698dcd2b002b84962aa
janzheng <janeazy@gmail.com>
2017-10-11 02:31:20 -0400

    wip work

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -162,3 +162,23 @@ function onLoadPage() {
 }
 
 
+
+
+
+
+
+
+// css/js hybrid
+// dynamic height dispenser
+// don't rely on vh as browser bugs and annoyances like iOS chrome
+// vw more reliable
+// syntax: class="_size" data-height="30" is calculated 30 vh
+$(window).load(function() {
+  $('._size').each(function() {
+    // calculate vh manually, and set it dynamically
+    // console.log($(this).data())
+    var height = $(window).height() * ($(this).data('height') * 0.01);
+    $(this).css({'height': height})
+  });
+});
+
